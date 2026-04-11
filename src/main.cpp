c8a948d26983d3eea9643e78ac92b6348c1d2d35
zihad22 <mdzihad2244@gmail.com>
2026-04-11 15:20:27 +0600

    air pump work good

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,25 @@
 #include <LiquidCrystal_I2C.h>
 #include <SimpleDHT.h>
 
-int buttonPin = 9;
-int lastButtonState = HIGH;
+// relay
+int airPumpRelayPin = 11;
+int airPumpState = HIGH;
 
-int menu = 1;
+int waterPumpRelayPin = 10;
+int waterPumpState = HIGH;
+
+// menu
+int menuButtonPin = 8;
+int menuButtonState = HIGH;
+int menu = 5;
+
+// setting
+int settingButtonPin = 9;
+int settingButtonSate = HIGH;
 
 // debounce
-unsigned long lastPressTime = 0;
+unsigned long lastMenuPressTime = 0;
+unsigned long lastSettingPressTime = 0;
 int debounceDelay = 200;
 
 // sensors
@@ -26,46 +38,94 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 void setup()
 {
   Serial.begin(9600);
-  pinMode(buttonPin, INPUT_PULLUP);
+
+  // relay
+  pinMode(airPumpRelayPin, OUTPUT);
+  pinMode(waterPumpRelayPin, OUTPUT);
+
+  // menu
+  pinMode(menuButtonPin, INPUT_PULLUP);
+
+  // setting
+  pinMode(settingButtonPin, INPUT_PULLUP);
+
+  // lcd
   lcd.init();
   lcd.backlight();
 }
 
 void loop()
 {
-  int currentState = digitalRead(buttonPin);
 
-  if (lastButtonState == HIGH && currentState == LOW)
+  if (airPumpState == HIGH && waterPumpState == HIGH)
   {
-    if (millis() - lastPressTime > debounceDelay)
+    digitalWrite(airPumpRelayPin, HIGH);
+    digitalWrite(waterPumpRelayPin, HIGH);
+  }
+
+  // menu
+  int currentMenuState = digitalRead(menuButtonPin);
+
+  if (menuButtonState == HIGH && currentMenuState == LOW)
+  {
+    if (millis() - lastMenuPressTime > debounceDelay)
     {
       menu++;
-      if (menu > 4)
+      if (menu > 5)
+      {
         menu = 1;
-
-      Serial.print("Menu: ");
-      Serial.println(menu);
-
+      }
       lcd.clear();
-      lastPressTime = millis();
+      lastMenuPressTime = millis();
     }
   }
 
-  lastButtonState = currentState;
+  menuButtonState = currentMenuState;
 
+  // setting
+  int currentSettingState = digitalRead(settingButtonPin);
+
+  if (settingButtonSate == HIGH && currentSettingState == LOW)
+  {
+    if (millis() - lastSettingPressTime > debounceDelay)
+    {
+
+      if (airPumpState == HIGH && menu == 5)
+      {
+        airPumpState = LOW;
+        digitalWrite(airPumpRelayPin, LOW);
+      }
+      else
+      {
+        airPumpState = HIGH;
+        digitalWrite(airPumpRelayPin, HIGH);
+      }
+
+      Serial.print("button Click");
+      Serial.println(currentSettingState);
+
+      lastSettingPressTime = millis();
+    }
+  }
+
+  settingButtonSate = currentSettingState;
+
+  // water level
   waterLevel = analogRead(waterLevelPin);
 
+  // turbidity
   turbidityValue = analogRead(turbidityPin);
   int turbidity = map(turbidityValue, 600, 750, 100, 0);
   turbidity = constrain(turbidity, 0, 100);
 
+  // dht11
   byte temperature = 0;
   byte humidity = 0;
-
   dht11.read(&temperature, &humidity, NULL);
 
   if (menu == 1)
   {
+    // dh11
     lcd.setCursor(0, 0);
     lcd.print("Temp: ");
     lcd.print((int)temperature);
@@ -79,6 +139,7 @@ void loop()
 
   else if (menu == 2)
   {
+    // water level
     lcd.setCursor(0, 0);
     lcd.print("Water Level");
 
@@ -94,6 +155,7 @@ void loop()
 
   else if (menu == 3)
   {
+    // turbidity
     lcd.setCursor(0, 0);
     lcd.print("Turbidity: ");
     lcd.print(turbidity);
@@ -115,11 +177,33 @@ void loop()
   else if (menu == 4)
   {
     lcd.setCursor(0, 0);
-    lcd.print("Settings");
+    lcd.print("Settings: ");
 
     lcd.setCursor(0, 1);
-    lcd.print("Coming Soon...");
+    lcd.print("Click to go next");
+  }
+  else if (menu == 5)
+  {
+    // air pump on/of setting
+
+    lcd.setCursor(0, 0);
+    lcd.print("Air Pump: ");
+    lcd.setCursor(10, 0);
+    if (airPumpState == HIGH)
+    {
+      lcd.print("OFF");
+      lcd.setCursor(0, 1);
+      lcd.print("Click to on     ");
+    }
+    else
+    {
+      lcd.print("ON ");
+      lcd.setCursor(0, 1);
+      lcd.print("Click to go next");
+    }
+
+    lcd.setCursor(0, 1);
   }
 
-  delay(200); // small refresh
+  delay(200);
 }
